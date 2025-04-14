c22224cdea504b14210efe42562566dd1612128b
CtrlZ233 <328825619@qq.com>
2025-04-14 16:26:58 +0800

    [test] add usertest framework

--- src/bin/test_hello.rs
+++ src/bin/test_hello.rs
@@ -7,7 +7,6 @@ use mork_user_lib::mork_task::mork_thread_suspend;
 
 #[unsafe(no_mangle)]
 pub fn main() {
-    mork_user_lib::init();
-    mork_user_log!(info, "mork test hello1!");
+    mork_user_log!(info, "mork test hello!");
     mork_thread_suspend(CNodeSlot::CapInitThread as usize).unwrap();
 }
