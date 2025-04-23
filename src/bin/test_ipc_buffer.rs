fb8188df6bc7bc3105391f3297b7c02a7b40b474
CtrlZ233 <328825619@qq.com>
2025-04-23 20:54:26 +0800

    [user lib] add thread create api

--- src/bin/test_ipc_buffer.rs
+++ src/bin/test_ipc_buffer.rs
@@ -3,26 +3,13 @@
 
 use mork_common::mork_user_log;
 
-const MAIN_IPC_BUFFER_ADDR: usize = 0x1000_0000;
-
-use mork_common::constants::CNodeSlot;
-use mork_user_lib::mork_tls::tls_init;
-use mork_user_lib::mork_ipc_buffer::{ipc_buffer_init, with_ipc_buffer, with_ipc_buffer_mut};
+use mork_user_lib::mork_ipc_buffer::{with_ipc_buffer, with_ipc_buffer_mut};
 
 const SET_IPC_USER_DATA: usize = 100;
 
 #[unsafe(no_mangle)]
 pub fn main() {
-    mork_user_log!(info, "test thread create!");
-    if let Err(_) = tls_init() {
-        mork_user_log!(error, "Failed to initialize TLS!");
-        return;
-    }
-    mork_user_log!(info, "Successfully initialized TLS!");
-    if let Err(_) = ipc_buffer_init(CNodeSlot::CapInitThread as usize, MAIN_IPC_BUFFER_ADDR) {
-        mork_user_log!(error, "Failed to initialize IPC context!");
-        return;
-    }
+    mork_user_log!(info, "test IPC Buffer!");
 
     with_ipc_buffer_mut(|buffer| {
         buffer.user_data = SET_IPC_USER_DATA;
--- src/bin/test_thread_create.rs
+++ src/bin/test_thread_create.rs
@@ -2,47 +2,32 @@
 #![no_main]
 
 use core::sync::atomic::AtomicUsize;
-
+use core::sync::atomic::Ordering::Relaxed;
 use mork_common::{constants::CNodeSlot, mork_user_log};
 use mork_user_lib::mork_ipc::mork_notification_signal;
-use mork_user_lib::mork_ipc_buffer::ipc_buffer_init;
-use mork_user_lib::mork_task::{create_thread, mork_thread_suspend};
-use mork_user_lib::mork_tls::tls_init;
-
-const MAIN_IPC_BUFFER_ADDR: usize = 0x1000_0000;
-const MAX_THREAD_NUM: usize = 512;
+use mork_user_lib::mork_task::mork_task_suspend;
+use mork_user_lib::mork_thread::create_thread;
+const MAX_THREAD_NUM: usize = 1000;
 
 static COUNT: AtomicUsize = AtomicUsize::new(0);
 
 pub fn child_thread() {
-    // mork_user_log!(info, "Hello from child thread!");
-    COUNT.fetch_add(1, core::sync::atomic::Ordering::Relaxed);
+    COUNT.fetch_add(1, Relaxed);
     mork_notification_signal(CNodeSlot::CapParentCom as usize).unwrap();
-    mork_thread_suspend(CNodeSlot::CapInitThread as usize).unwrap();
+    mork_task_suspend(CNodeSlot::CapInitThread as usize).unwrap();
 }
 
 #[unsafe(no_mangle)]
-pub fn main() {
+pub fn main()  {
     mork_user_log!(info, "test thread create!");
-    if let Err(_) = tls_init() {
-        mork_user_log!(error, "Failed to initialize TLS!");
-        return;
-    }
-    mork_user_log!(info, "Successfully initialized TLS!");
-    if let Err(_) = ipc_buffer_init(CNodeSlot::CapInitThread as usize, MAIN_IPC_BUFFER_ADDR) {
-        mork_user_log!(error, "Failed to initialize IPC context!");
-        return;
-    }
-
     for _ in 0..MAX_THREAD_NUM {
         if let Ok(child) = create_thread(child_thread as usize) {
             child.resume().unwrap();
             child.wait().unwrap();
             child.free().unwrap();
         } else {
-            mork_user_log!(error, "Failed to create child thread!");
-            return;
+            panic!("Failed to create child thread!");
         }
     }
-    assert_eq!(COUNT.load(core::sync::atomic::Ordering::Relaxed), MAX_THREAD_NUM);
+    assert_eq!(COUNT.load(Relaxed), MAX_THREAD_NUM);
 }
